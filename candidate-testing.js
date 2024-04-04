9ffd062a04f06255ce9292a5e11adf5de18b23b5
unknown <augolmstead17@gmail.com>
2024-04-04 12:03:49 -0500

    Final Candidate Testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,7 +13,7 @@ let candidateAnswer = ""
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"]
-let candidateAnswers = ""
+let candidateAnswers = []
 
 
 function askForName() {
@@ -22,34 +22,41 @@ function askForName() {
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  let candidateAnswerList = [] 
+
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer // 
+  
   for (i=0; i < questions.length; i++) {
     candidateAnswer = input.question(questions[i])
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
     
-  }
 
-}
+    if (candidateAnswer == correctAnswers[i]) {
+      console.log("Yes, that's right!")}      
+      else {
+      console.log("Oh, that's wrong.")
+      }
+
+      candidateAnswers.push(candidateAnswer)
+} 
+} 
 function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer == correctAnswer) {
-    console.log("Yes, that's right!")
-} else {
-    console.log("That's wrong. Try again.")
+  let grade = 0  //TODO 3.2 use this variable to calculate the candidates score.
+  for (i=0; i < questions.length; i++) {
+    if (candidateAnswers[i] == correctAnswers[i]) {
+      grade = (grade + 20)
+}     else {
+      grade = (grade + 0)
 }
-
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
+}
+    console.log(`\nWell done. Let's see how you did.\n\nYOU SCORED ${grade}%!\n\nAMAZING!!!`)
   return grade;
-}
+  }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log(`${candidateName}... That's a great name. It's nice to meet you.\nNow prove yourself.`);
+   console.log(`${candidateName}... That's a great name. It's nice to meet you.\nNow prove yourself.\n`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
